53032c5c43f8137dd5237bbdc51fa94faa459eb5
yxy <session66@163.com>
2022-10-13 20:55:54 +0800

    导航守卫和登录完成

--- src/permission.js
+++ src/permission.js
@@ -1,16 +1,36 @@
 import router from './router'
-
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
+import NProgress from 'nprogress' // 导入进度条插件
+import 'nprogress/nprogress.css' // 导入进度条样式
+import store from '@/store'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-// const whiteList = ['/login'] // no redirect whitelist
+// 白名单数组  // 白名单: 无需登录,可以跳转查看的路由地址(必须在路由表里)
+const whiteList = ['/login', '/404']
 
+// 前置导航守卫
 router.beforeEach(async(to, from, next) => {
-  next()
+  NProgress.start()
+  const token = store.getters.token
+  if (token) { // 登陆了
+    if (to.path === '/login') { // 去登录页
+      // 中断要跳转 /login 这次导航，重新跳转到/(首页)
+      next('/')
+      NProgress.done()
+    } else { // 去别的页面
+      next()
+    }
+  } else { // 没有登陆
+    if (whiteList.includes(to.path)) { // 要去的路由地址字符串，是否在白名单数组里出现过，如果出现过就放行
+      next()
+    } else { // 去别的页面
+      next('/login')
+      NProgress.done()
+    }
+  }
 })
 
+// 后置导航守卫
 router.afterEach(() => {
-
+  NProgress.done()
 })
