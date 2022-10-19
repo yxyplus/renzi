8565c272db76917512887956c8943830cf74259d
yxy <session66@163.com>
2022-10-19 15:00:08 +0800

    员工-添加员工-表单校验

--- src/views/employees/components/empForm.vue
+++ src/views/employees/components/empForm.vue
@@ -1,30 +1,30 @@
 <template>
   <!-- 表单 -->
-  <el-form label-width="120px">
-    <el-form-item label="姓名">
-      <el-input style="width:50%" placeholder="请输入姓名" />
+  <el-form ref="addForm" :model="formData" :rules="rules" label-width="120px">
+    <el-form-item label="姓名" prop="username">
+      <el-input v-model="formData.username" style="width:50%" placeholder="请输入姓名" />
     </el-form-item>
-    <el-form-item label="手机">
-      <el-input style="width:50%" placeholder="请输入手机号" />
+    <el-form-item label="手机" prop="mobile">
+      <el-input v-model="formData.mobile" style="width:50%" placeholder="请输入手机号" />
     </el-form-item>
-    <el-form-item label="入职时间">
-      <el-date-picker style="width:50%" placeholder="请选择入职时间" />
+    <el-form-item label="入职时间" prop="timeOfEntry">
+      <el-date-picker v-model="formData.timeOfEntry" style="width:50%" placeholder="请选择入职时间" />
     </el-form-item>
-    <el-form-item label="聘用形式">
-      <el-select style="width:50%" placeholder="请选择" value="" />
+    <el-form-item label="聘用形式" prop="formOfEmployment">
+      <el-select v-model="formData.formOfEmployment" style="width:50%" placeholder="请选择" />
     </el-form-item>
-    <el-form-item label="工号">
-      <el-input style="width:50%" placeholder="请输入工号" />
+    <el-form-item label="工号" prop="workNumber">
+      <el-input v-model="formData.workNumber" style="width:50%" placeholder="请输入工号" />
     </el-form-item>
-    <el-form-item label="部门">
-      <el-input style="width:50%" placeholder="请选择部门" />
+    <el-form-item label="部门" prop="departmentName">
+      <el-input v-model="formData.departmentName" style="width:50%" placeholder="请选择部门" />
     </el-form-item>
-    <el-form-item label="转正时间">
-      <el-date-picker style="width:50%" placeholder="请选择转正时间" />
+    <el-form-item label="转正时间" prop="correctionTime">
+      <el-date-picker v-model="formData.correctionTime" style="width:50%" placeholder="请选择转正时间" />
     </el-form-item>
     <el-form-item>
-      <el-button @click="cancelFn">取消</el-button>
-      <el-button type="primary" @click="confirmFn">确定</el-button>
+      <el-button @click="addCancel">取消</el-button>
+      <el-button type="primary" @click="addSubmit">确定</el-button>
     </el-form-item>
   </el-form>
 </template>
@@ -32,12 +32,54 @@
 <script>
 export default {
   name: 'EmpForm',
+  data() {
+    return {
+    // 添加表单字段
+      formData: {
+        username: '', // 用户名
+        mobile: '', // 手机号
+        formOfEmployment: '', // 聘用形式
+        workNumber: '', // 工号
+        departmentName: '', // 部门
+        timeOfEntry: '', // 入职时间
+        correctionTime: '' // 转正时间
+      },
+      rules: {
+        username: [
+          { required: true, message: '用户姓名不能为空', trigger: 'blur' },
+          { min: 1, max: 4, message: '用户姓名为1~4位', trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: 'blur' },
+          { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确', trigger: 'blur' }
+        ],
+        formOfEmployment: [
+          { required: true, message: '聘用形式不能为空', trigger: 'blur' }
+        ],
+        workNumber: [
+          { required: true, message: '工号不能为空', trigger: 'blur' }
+        ],
+        departmentName: [
+          { required: true, message: '部门不能为空', trigger: 'change' }
+        ],
+        timeOfEntry: [
+          { required: true, message: '请选择入职时间', trigger: 'blur' }
+        ]
+      }
+    }
+  },
   methods: {
-    cancelFn() {
+    // 取消->点击事件
+    addCancel() {
       this.$emit('update:showDialog', false)
     },
-    confirmFn() {
-      this.$emit('update:showDialog', false)
+    // 确定->点击事件
+    addSubmit() {
+      this.$refs.addForm.validate(valid => {
+        if (valid) {
+          this.$emit('update:showDialog', false)
+        }
+      })
     }
   }
 }
