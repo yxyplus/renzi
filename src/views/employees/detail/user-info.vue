cabc962e09b72a1b35a066706d451018dd2146c6
yxy <session66@163.com>
2022-10-21 11:15:08 +0800

    员工-查看详情-个人详情-标签和数据回显

--- src/views/employees/detail/user-info.vue
+++ src/views/employees/detail/user-info.vue
@@ -1,12 +1,106 @@
 <template>
   <div class="user-info">
-    个人信息
+    <!-- 个人信息 -->
+    <el-form label-width="220px">
+      <!-- 工号 入职时间 -->
+      <el-row class="inline-info">
+        <el-col :span="12">
+          <el-form-item label="工号">
+            <el-input v-model="userInfo.workNumber" class="inputW" />
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="12">
+          <el-form-item label="入职时间">
+            <el-date-picker
+              v-model="userInfo.timeOfEntry"
+              style="width: 300px"
+              type="date"
+              class="inputW"
+              value-format="YYYY-MM-DD"
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <!-- 姓名 部门 -->
+      <el-row class="inline-info">
+        <el-col :span="12">
+          <el-form-item label="姓名">
+            <el-input v-model="userInfo.username" class="inputW" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="部门">
+            <el-input v-model="userInfo.departmentName" class="inputW" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <!--手机 聘用形式  -->
+      <el-row class="inline-info">
+        <el-col :span="12">
+          <el-form-item label="手机">
+            <el-input v-model="userInfo.mobile" style="width: 300px" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="聘用形式">
+            <el-select v-model="userInfo.formOfEmployment" class="inputW">
+              <el-option
+                v-for="item in EmployeeEnum.hireType"
+                :key="item.id"
+                :label="item.value"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <!-- 员工照片 -->
+      <el-row class="inline-info">
+        <el-col :span="12">
+          <el-form-item label="员工头像">
+            <!-- 放置上传图片 -->
+
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <!-- 保存个人信息 -->
+      <el-row class="inline-info" type="flex" justify="center">
+        <el-col :span="12">
+          <el-button type="primary" @click="saveUser">保存更新</el-button>
+          <el-button @click="$router.back()">返回</el-button>
+        </el-col>
+      </el-row>
+    </el-form>
   </div>
 </template>
 
 <script>
+import { getUserPhotoAPI } from '@/api/user'
+import EmployeeEnum from '@/api/constant'
+
 export default {
-  name: 'UserInfo'
+  name: 'UserInfo',
+  data() {
+    return {
+      userInfo: {}, // 个人信息-对象
+      EmployeeEnum
+    }
+  },
+  created() {
+    // 请求->个人信息
+    this.getUserInfoFn()
+  },
+  methods: {
+    async getUserInfoFn() {
+      const res = await getUserPhotoAPI(this.$route.query.id)
+      this.userInfo = res.data
+    },
+    // 保存按钮更新->点击事件
+    saveUser() {
+
+    }
+  }
 }
 </script>
 
