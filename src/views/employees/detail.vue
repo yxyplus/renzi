3f168f9893c1abc8494f5809eff4d49a7614239e
yxy <session66@163.com>
2022-10-21 10:14:15 +0800

    员工-查看详情-3个子页面标签

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -3,11 +3,31 @@
     <div class="app-container">
       <el-card>
         <el-tabs>
-          <el-tab-pane label="登录账户设置" />
-          <el-tab-pane label="个人详情" />
-          <el-tab-pane label="岗位信息" />
+          <el-tab-pane label="登录账户设置">
+            <LoginSetting />
+          </el-tab-pane>
+          <el-tab-pane label="个人详情">
+            <UserInfo />
+          </el-tab-pane>
+          <el-tab-pane label="岗位信息">
+            <JobInfo />
+          </el-tab-pane>
         </el-tabs>
       </el-card>
     </div>
   </div>
 </template>
+
+<script>
+import JobInfo from './detail/job-info'
+import LoginSetting from './detail/login-setting'
+import UserInfo from './detail/user-info'
+
+export default {
+  components: {
+    JobInfo,
+    LoginSetting,
+    UserInfo
+  }
+}
+</script>
