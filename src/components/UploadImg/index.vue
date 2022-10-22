88ced328cb93e12e6706fb10850ee998607c1b30
yxy <session66@163.com>
2022-10-22 08:39:19 +0800

    员工-查看详情-个人详情-图片上传进度条显示

--- src/components/UploadImg/index.vue
+++ src/components/UploadImg/index.vue
@@ -11,6 +11,7 @@
     >
       <img v-if="imageUrl" :src="imageUrl" class="avatar">
       <i v-else class="el-icon-plus avatar-uploader-icon" />
+      <el-progress v-show="showProgress" type="circle" class="progress" :percentage="percentage" />
     </el-upload>
 
   </div>
@@ -27,8 +28,9 @@ export default {
   name: 'UploadImg',
   data() {
     return {
-      imageUrl: ''
-
+      imageUrl: '', // 上传成功的图片地址(外链)
+      showProgress: false, // 环形进度条
+      percentage: 0 // 上传进度(0-100的数字)
     }
   },
   methods: {
@@ -59,6 +61,7 @@ export default {
     // 进行上传操作
     upload(params) {
       //   console.log(params.file)
+      this.showProgress = true
       if (params.file) {
         // 执行上传操作
         cos.putObject(
@@ -68,15 +71,20 @@ export default {
             Key: params.file.name, // 文件名
             Body: params.file, // 要上传的文件对象
             StorageClass: 'STANDARD', // 上传的模式类型 直接默认 标准模式即可
-            // 上传到腾讯云 =》 哪个存储桶 哪个地域的存储桶 文件  格式  名称 回调
+            // 上传到腾讯云 -> 哪个存储桶 哪个地域的存储桶 文件  格式  名称 回调
             onProgress: (progressData) => {
-              console.log(JSON.stringify(progressData))
+              // progressData.percent -> 进度(0-1)数字
+              this.percentage = parseInt(progressData.percent * 100)
+              // console.log(JSON.stringify(progressData))
             }
           },
           (err, data) => {
             // data返回数据之后 应该如何处理
             console.log(err || data)
             this.imageUrl = 'http://' + data.Location
+            setTimeout(() => {
+              this.showProgress = false
+            }, 200)
           }
         )
       }
@@ -107,5 +115,17 @@ export default {
   width: 178px;
   height: 178px;
   display: block;
+}
+</style>
+<style>
+.progress {
+  position: absolute;
+  display: flex;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #e4e4e4;
+  border-radius: 50%;
+  opacity: 0.9;
 }
 </style>
